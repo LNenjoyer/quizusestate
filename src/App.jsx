8576a2a5e977e797ae39ca5218bc1aea2ff4494a
LNenjoyer <ut_3779@zsp3zamosc.pl>
2025-01-21 11:59:28 +0100

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,40 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() { 
-  const [isSpoilerShown, setIsSpoilerShown] = useState(false); 
-  const [isWarningShown, setIsWarningShown] = useState(true); 
+  // const [isSpoilerShown, setIsSpoilerShown] = useState(false); 
+  // const [isWarningShown, setIsWarningShown] = useState(true); 
+  const [isYes, setIsYes] = useState(false); 
+  const [isNo, setIsNo] = useState(false); 
+
   function handleShowSpoilerClick() { 
     setIsSpoilerShown(true); 
     setIsWarningShown(false); 
   } 
-  function handleCloseWarningClick() { 
-    setIsWarningShown(false); 
+  function handleYes() { 
+    setIsNo(false); 
+    setIsYes(true);
+  } 
+  function handleNo() { 
+    setIsYes(false); 
+    setIsNo(true); 
   } 
   return ( 
     <> 
-      <h1>Gwiezdne Wojny V</h1> 
-      <h2>Rok produkcji: 1980</h2> 
-      <h2>Fabuła</h2> 
-      {isWarningShown && ( 
-        <p> 
-          Uwaga! Opis fabuły zawiera spoilery! 
-          <button onClick={handleCloseWarningClick}>X</button> 
-        </p> 
-      )} 
-      <p>Dobro walczy ze złem. Trzeba wyłączyć pole siłowe.</p> 
-      {isSpoilerShown ? ( 
-        <p>Vader okazuje się być ojcem Luke'a.</p> 
+    <p>Pytanie quizu nr xd</p>
+
+      {isYes ? ( 
+        <p>Poprawmie</p> 
       ) : ( 
-        <button onClick={handleShowSpoilerClick}>Pokaż spoiler</button> 
+        <button onClick={handleYes}>Yes</button> 
+        
       )} 
+      {isNo ? ( 
+        <p>Jeszcze raz</p> 
+      ) : ( 
+        <button onClick={handleNo}>Yes</button> 
+        
+      )} 
+      
     </> 
   ); 
 } 
